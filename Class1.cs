a784715a6a5b4d3dbdc5088350b02a9fcfe2ad72
javad es <javad.eslami73@gmail.com>
2020-12-11 09:24:33 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    public static class MergeClass
+    {
+        public static void DomergeSort(this int[] Numbers)
+        {
+            var SortedNumbers = mergeSort(Numbers);
+            for(int i=0;i<SortedNumbers.Length;i++)
+            {
+                Numbers[i] = SortedNumbers[i];
+            }
+        }
+        private static int[] mergeSort(int[] Numbers)
+        {
+            if (Numbers.Length <= 1)//base case
+            {
+                return Numbers;
+            }
+
+            var left = new List<int>();
+            var right = new List<int>();
+
+            for (int i=0;i<Numbers.Length;i++)
+            {
+                if (i % 2 > 0)
+                {
+                    left.Add(Numbers[i]);
+                }
+                else
+                {
+                    right.Add(Numbers[i]);
+                }
+
+            }
+
+            left = mergeSort(left.ToArray()).ToList();
+            right = mergeSort(right.ToArray()).ToList();
+
+            return merge(left, right);
+        }
+        private static int[] merge(List<int> left, List<int> right)
+        {
+            var result = new List<int>();
+            while(left.Count>0 && right.Count>0)
+            {
+                if (left.First() <= right.First())
+                    MoveValuesFromSourceToResult(left, result);
+                    MoveValuesFromSourceToResult(right, result);
+            }
+
+            while(left.Count>0)
+            {
+                MoveValuesFromSourceToResult(left, result);
+            }
+            while (right.Count > 0)
+            {
+                MoveValuesFromSourceToResult(right, result);
+            }
+
+            return result.ToArray();
+        }
+
+        private static void MoveValuesFromSourceToResult(List<int> list, List<int> result)
+        {
+            result.Add(list.First());
+            list.RemoveAt(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMergeSort();
+
+            Console.ReadKey();
+        }
+
+         private static void TestMergeSort()
+        {
+            Random r = new Random();
+
+            int[] Numbers = new int[10];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = r.Next(10, 100);
+            }
+
+            Console.WriteLine(" Unsorted array: ");
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.Write(Numbers[i] + "  ");
+            }
+
+            Console.WriteLine("\nSorted array: ");
+            Numbers.DomergeSort();
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.Write(Numbers[i] + "  ");
+            }
+        }
+    }
+    
+}
